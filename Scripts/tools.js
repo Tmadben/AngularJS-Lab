6a6008f2fca6e540d8be472e2d10c7119e46b881
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-07-14 13:31:30 +0000

    Add new customer in the table

--- Scripts/tools.js
+++ Scripts/tools.js
@@ -1,56 +1,49 @@
-var listCustomers = [];
-var initCustomer = function(){
-    return listCustomers=
-    [
-            {
-                "firstName": "JOHANNA",
-                "familyName": "VICENT",
-                "gender": "F",
-                "age": 50
-            },
-            {
-                "firstName": "CECILIA",
-                "familyName": "FRANK",
-                "gender": "F",
-                "age": 18
-            },
-            {
-                "firstName": "BERNARD",
-                "familyName": "COUTEAU",
-                "gender": "M",
-                "age": 60
-            },
-            {
-                "firstName": "ALAIN",
-                "familyName": "LE SAC",
-                "gender": "M",
-                "age": 34
-            },
-            {
-                "firstName": "YVES",
-                "familyName": "YDE JACQUES",
-                "gender": "M",
-                "age": 45
-            },
-            {
-                "firstName": "LUCIE",
-                "familyName": "CARTELLE",
-                "gender": "F",
-                "age": 49
-            }
-    ];
+var customerApp = angular.module("customerApp", []);
 
+customerApp.controller('customersController', function ($scope) {
+   $scope.listCustomers =  [
+           {
+               "firstName": "JOHANNA",
+               "familyName": "VICENT",
+               "gender": "F",
+               "age": 50
+           },
+           {
+               "firstName": "CECILIA",
+               "familyName": "FRANK",
+               "gender": "F",
+               "age": 18
+           },
+           {
+               "firstName": "BERNARD",
+               "familyName": "COUTEAU",
+               "gender": "M",
+               "age": 60
+           },
+           {
+               "firstName": "ALAIN",
+               "familyName": "LE SAC",
+               "gender": "M",
+               "age": 34
+           },
+           {
+               "firstName": "YVES",
+               "familyName": "YDE JACQUES",
+               "gender": "M",
+               "age": 45
+           },
+           {
+               "firstName": "LUCIE",
+               "familyName": "CARTELLE",
+               "gender": "F",
+               "age": 49
+           }
+   ];
 
-}
-var addNewCustomer = function(){
-
-       var customerObject = {
-        "firstName": $scope.firstName,
-        "familyName": $scope.familyName,
-        "gender": $scope.gender,
-        "age": $scope.age
+    $scope.addNewCustomer = function(customer) {
+        $scope.listCustomers.push(customer);
+        customer = {};
+        
     }
 
-    listCustomers.push(customerObject);
-
-};
+});
