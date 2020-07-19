8d0bd90d1b0aaaf1c4eff398c54ea4ddc88e5e9d
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-07-19 12:43:44 +0000

    Complete listAllCustomers.js controller

--- controller/listAllCustomers.js
+++ controller/listAllCustomers.js
@@ -1,1 +1,44 @@
 //List of all the customers
+var customerApp = angular.module("customerApp", []);
+
+customerApp.controller('customersController', function ($scope) {
+   $scope.listCustomers =  [
+           {
+               "firstName": "JOHANNA",
+               "familyName": "VICENT",
+               "gender": "F",
+               "age": 50
+           },
+           {
+               "firstName": "CECILIA",
+               "familyName": "FRANK",
+               "gender": "F",
+               "age": 18
+           },
+           {
+               "firstName": "BERNARD",
+               "familyName": "COUTEAU",
+               "gender": "M",
+               "age": 60
+           },
+           {
+               "firstName": "ALAIN",
+               "familyName": "LE SAC",
+               "gender": "M",
+               "age": 34
+           },
+           {
+               "firstName": "YVES",
+               "familyName": "YDE JACQUES",
+               "gender": "M",
+               "age": 45
+           },
+           {
+               "firstName": "LUCIE",
+               "familyName": "CARTELLE",
+               "gender": "F",
+               "age": 49
+           }
+   ];
+
+});
