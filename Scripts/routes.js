209fac69cff2bde561532c5079b60994f79dc4fe
Mamadou Ben TRAORE <tmadben@gmail.com>
2020-07-19 12:14:42 +0000

    Add mainViewController.js

--- Scripts/routes.js
+++ Scripts/routes.js
@@ -5,22 +5,26 @@ customerApp.config(function($routeProvider){
     $routeProvider
         .when('/',
             {
-
+                controller:
+                templateUrl:
             }
         )
         .when('/addnewcustomer',
             {
-                
+                controller:
+                templateUrl:
             }
         )
         .when('/detailscustomer',
             {
-                
+                controller:
+                templateUrl:
             }
         )
         .when('/listallcustomers',
             {
-                
+                controller:
+                templateUrl:
             }
         )
         .otherwise({ redirectTo: '/' })
